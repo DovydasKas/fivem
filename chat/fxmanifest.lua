e7df45abd8111c3df1f3755b30c3fe857ef96c08
Dovydas <dovydas.email159@gmail.com>
2022-02-07 19:52:13 +0200

    chat edit

--- chat/fxmanifest.lua
+++ chat/fxmanifest.lua
@@ -1,45 +1,30 @@
-fx_version 'cerulean'
-game 'gta5'
+-- This resource is part of the default Cfx.re asset pack (cfx-server-data)
+-- Altering or recreating for local use only is strongly discouraged.
 
-ui_page 'html/index.html'
+version '1.0.0'
+author 'Cfx.re <root@cfx.re>'
+description 'Provides baseline chat functionality using a NUI-based interface.'
+repository 'https://github.com/citizenfx/cfx-server-data'
+
+ui_page 'dist/ui.html'
+
+client_script 'cl_chat.lua'
+server_script 'sv_chat.lua'
 
 files {
-  'html/index.html',
-  'html/css/index.css',
-  'html/js/config.default.js',
-  'html/js/config.js',
-  'html/js/App.js',
-  'html/js/Message.js',
-  'html/js/Suggestions.js',
-  'html/vendor/vue.2.3.3.min.js',
-  'html/vendor/flexboxgrid.6.3.1.min.css',
-  'html/vendor/animate.3.5.2.min.css',
-  'html/vendor/latofonts.css',
-  'html/vendor/fonts/LatoRegular.woff2',
-  'html/vendor/fonts/LatoRegular2.woff2',
-  'html/vendor/fonts/LatoLight2.woff2',
-  'html/vendor/fonts/LatoLight.woff2',
-  'html/vendor/fonts/LatoBold.woff2',
-  'html/vendor/fonts/LatoBold2.woff2',
-  'html/css/style.css',
+  'dist/ui.html',
+  'dist/index.css',
+  'html/vendor/*.css',
+  'html/vendor/fonts/*.woff2',
 }
 
-client_scripts {
-  'client/commands.lua',
-  'client/cl_chat.lua',
+fx_version 'adamant'
+games { 'rdr3', 'gta5' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+dependencies {
+  'yarn',
+  'webpack'
 }
 
-server_scripts {
-  '@mysql-async/lib/MySQL.lua',
-  'server/utils.lua',
-  'server/commands.lua',
-  'server/sv_chat.lua',
-  'server/main.lua',
-}
-
-chat_theme 'gtao' {
-    styleSheet = 'style.css',
-    msgTemplates = {
-        default = '<b>{0}</b><span>{1}</span>'
-    }
-}
+webpack_config 'webpack.config.js'
