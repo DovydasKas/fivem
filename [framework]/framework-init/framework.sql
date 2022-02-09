b77502b4d80fd7100fded58e7eadc69987ac9df6
Dovydas <dovydas.email159@gmail.com>
2022-02-09 17:14:20 +0200

    ped edit, DB

--- [framework]/framework-init/framework.sql
+++ [framework]/framework-init/framework.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `fivem`;
 USE `fivem`;
 
-CREATE TABLE `user_identifiers` (
+CREATE TABLE IF NOT EXISTS `user_identifiers` (
     `steamname` VARCHAR(50) NOT NULL,
     `steamid` VARCHAR(50) NOT NULL,
     `license` VARCHAR(50) NOT NULL,
@@ -11,12 +11,26 @@ CREATE TABLE `user_identifiers` (
     PRIMARY KEY(`steamid`)
 );
 
-CREATE TABLE `user_info` (
+CREATE TABLE IF NOT EXISTS `user_info` (
     `steamname` VARCHAR(50) NOT NULL,
     `steamid` VARCHAR(50) NOT NULL,
     `position` VARCHAR(50) NULL DEFAULT '{-1079.974, -500.2022, 36.57581}',
     PRIMARY KEY (`steamid`),
     CONSTRAINT `FK_user_info_user_identifiers`
+    FOREIGN KEY (`steamid`)
+    REFERENCES `user_identifiers` (`steamid`)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+
+
+CREATE TABLE IF NOT EXISTS `user_weapons` (
+    `steamid` VARCHAR(100) NOT NULL,
+    `weapon` VARCHAR(100) NULL DEFAULT NULL,
+    `ammo` INT(50),
+    PRIMARY KEY (`steamid`),
+    CONSTRAINT `FK_user_weapons_user_identifiers`
     FOREIGN KEY (`steamid`)
     REFERENCES `user_identifiers` (`steamid`)
     ON UPDATE CASCADE
---
+++ [small things]/riot/client.lua
@@ -0,0 +1,18 @@
+
+SetRiotModeEnabled(true)
+
+RegisterCommand("riot", function(source, args)
+    local var = args[1]
+    if var == 'enable' then
+        SetRiotModeEnabled(true)
+        TriggerEvent('chat:addMessage', {
+            args = {'Riot enabled'}
+        })
+    elseif var == 'disable' then
+        SetRiotModeEnabled(false)
+        TriggerEvent('chat:addMessage', {
+            args = {'Riot disabled'}
+        })
+    end
+
+end)
